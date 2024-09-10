8e9c03fd5df9dfcf4033f1db71dc3d5613306561
Gorazd Biskoski <gorazdbiskoskii@gmail.com>
2024-09-10 14:32:42 +0200

    Add all functionalities for Admin view.

--- cookcraft-app/src/components/AdminViewComponents/AdminReviewCard.js
+++ cookcraft-app/src/components/AdminViewComponents/AdminReviewCard.js
@@ -1,18 +1,23 @@
 import styles from "../../css/AdminPanelCss/admin-style.module.css";
 
-const AdminReviewCard = ( { data } ) => {
-
+const AdminReviewCard = ({ data, onClick }) => {
     const { userName, userSurname, recipeName, rating } = data;
 
-    return <>
-        <div className={styles.card}>
+    return (
+        <div className={styles.card} onClick={onClick}>
             <div className={styles.cardContent}>
-                <h3 className={styles.cardTitle}>{userName} {userSurname}</h3>
-                <p><strong>Recipe:</strong> {recipeName} </p>
-                <p><strong>Rating:</strong> {rating} </p>
+                <h3 className={styles.cardTitle}>
+                    {userName} {userSurname}
+                </h3>
+                <p>
+                    <strong>Recipe:</strong> {recipeName}
+                </p>
+                <p>
+                    <strong>Rating:</strong> {rating}
+                </p>
             </div>
         </div>
-    </>
-}
+    );
+};
 
 export default AdminReviewCard;
--- cookcraft-app/src/components/AdminViewComponents/ApplicationCard.js
+++ cookcraft-app/src/components/AdminViewComponents/ApplicationCard.js
@@ -1,54 +1,54 @@
 import styles from "../../css/AdminPanelCss/admin-style.module.css";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-const ApplicationCard = ( { data } ) => {
-    const [userData, setUserData] = useState({})
+const ApplicationCard = ({ data, onClick }) => {
+    const [userData, setUserData] = useState({});
 
     useEffect(() => {
-        const url = 'http://localhost:8080/api';
-        const { cv, motivationalLetter, userId} = data;
-        //TODO Show modal with cv and motivational letter
-        // console.log(cv)
-        // console.log(motivationalLetter)
+        const url = "http://localhost:8080/api";
+        const { userId } = data;
+
         const fetchUserData = async () => {
             try {
                 const token = localStorage.getItem("token");
                 const response = await fetch(`${url}/users/${userId}`, {
-                    method: 'GET',
+                    method: "GET",
                     headers: {
-                        'Content-Type': "application/json",
-                        'Authorization': `Bearer ${token}`
-                    }
-                })
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
 
-                if(response.ok)
-                {
+                if (response.ok) {
                     const user_data = await response.json();
-                    setUserData(user_data)
-                }
-                else
-                {
+                    setUserData(user_data);
+                } else {
                     alert("Custom response.");
                 }
-            }
-            catch (error) {
+            } catch (error) {
                 console.error(error);
                 alert("Error communicating with the server.");
             }
-        }
+        };
 
-        fetchUserData()
+        fetchUserData();
     }, [data]);
 
-    return <>
-        <div className={styles.card}>
+    const handleClick = () => {
+        onClick({ ...data, userData });
+    };
+
+    return (
+        <div className={styles.card} onClick={handleClick}>
             <div className={styles.cardContent}>
-                <h3 className={styles.cardTitle}>{userData.userName} {userData.userSurname}</h3>
+                <h3 className={styles.cardTitle}>
+                    {userData.userName} {userData.userSurname}
+                </h3>
                 <p><strong>Email:</strong> {userData.email}</p>
                 <p><strong>Phone Number:</strong> {userData.phoneNumber}</p>
             </div>
         </div>
-    </>
-}
+    );
+};
 
 export default ApplicationCard;
